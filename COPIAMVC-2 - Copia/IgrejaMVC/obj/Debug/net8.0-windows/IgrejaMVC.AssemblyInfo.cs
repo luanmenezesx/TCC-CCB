0e86437350e71c99b357340317e2795a061e3ba2
luanmenezesx <admin@LICLABTEC-14.adserver.unisantos.br>
2025-06-03 19:30:12 -0300

    Modificando word

--- COPIAMVC-2 - Copia/IgrejaMVC/obj/Debug/net8.0-windows/IgrejaMVC.AssemblyInfo.cs
+++ COPIAMVC-2 - Copia/IgrejaMVC/obj/Debug/net8.0-windows/IgrejaMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IgrejaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59529a23a0e0d57097b61ca211cdca6db7b84f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a60d1b256dde3fa93ac95c3cb81d1e9e93d3c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("IgrejaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IgrejaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
